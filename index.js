cf158cd8d1105bf1cf612768103210b89a953be5
Luiz Fernando <fernandoaff1@gmail.com>
2019-03-29 07:02:13 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const vol = require("vol")
+const gamepad = require("gamepad")
+
+
+const VOLUME_UP = 7
+const VOLUME_DOWN = 5
+const VOL_INCREMENT = .03
+let currentVolume = 1;
+
+
+vol.get().then(start)
+
+function start(volume){
+    currentVolume = volume
+    showCurrentVolume()
+    gamepad.init()
+
+    for(var i = 0, l = gamepad.numDevices(); i<l; i++) console.log(i, gamepad.deviceAtIndex(i))
+    
+    setInterval(gamepad.processEvents, 16)
+    setInterval(gamepad.detectDevices, 500)
+    
+    
+    gamepad.on("down", (joystickId, buttonId) => {
+        switch(buttonId){
+            case VOLUME_UP:
+                volumeUp()
+                break
+            case VOLUME_DOWN:
+                volumeDown()
+                break
+        }
+    })
+}
+
+
+function volumeUp(){
+    incrementVolume(VOL_INCREMENT)
+}
+
+function volumeDown(){
+    incrementVolume(VOL_INCREMENT * -1)
+}
+
+function incrementVolume(value){
+    currentVolume += value
+    currentVolume = clamp(currentVolume)
+    vol.set(currentVolume)
+    showCurrentVolume()
+}
+
+function clamp(value, min = 0, max = 1){
+    return Math.min(max, Math.max(min, value))
+}
+
+function showCurrentVolume(){
+    console.log("Current volume: " + currentVolume)
+}
